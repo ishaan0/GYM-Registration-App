a45c9f2efe63ef25927ba21af34ec9ae694f5ef8
Muhammad Ishaan <iftekharctgbd@gmail.com>
2023-06-07 23:48:24 +0600

    registration form's some field added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +25,22 @@ import { UserDetailsComponent } from './user-details/user-details.component';
     RegistrationListComponent,
     UserDetailsComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/create-registration/create-registration.component.ts
+++ src/app/create-registration/create-registration.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-registration',
   templateUrl: './create-registration.component.html',
-  styleUrls: ['./create-registration.component.scss']
+  styleUrls: ['./create-registration.component.scss'],
 })
-export class CreateRegistrationComponent {
+export class CreateRegistrationComponent implements OnInit {
+  registrationForm!: FormGroup;
+  packages: string[] = ['Monthly', 'Quarterly', 'Yearly'];
+  importantList: string[] = [
+    'Toxic Fat reduction',
+    'Energy and Endurance',
+    'Building Lean Muscle',
+    'Healthier Digestive System',
+    'Sugar Craving Body',
+    'Fitness',
+  ];
+  constructor(private formBuilder: FormBuilder) {}
 
+  ngOnInit(): void {
+    this.registrationForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      mobile: ['', [Validators.required, Validators.pattern('^[0-9]{11}$')]],
+      weight: ['', Validators.required],
+      height: ['', Validators.required],
+      requireTrainer: [''],
+      gender: [''],
+      package: [''],
+      important: [''],
+    });
+  }
 }
